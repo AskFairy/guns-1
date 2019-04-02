c97c9a1422a8119bf6da99d24033646a45c599ff
chong.chen <chenchong0817@163.com>
2019-04-02 12:42:26 +0800

    去掉无用表

--- sql/guns.sql
+++ sql/guns.sql
@@ -74,20 +74,6 @@ INSERT INTO `sys_dict` VALUES ('36', '1', '35', '启用', null);
 INSERT INTO `sys_dict` VALUES ('37', '2', '35', '冻结', null);
 INSERT INTO `sys_dict` VALUES ('38', '3', '35', '已删除', null);
 
--- ----------------------------
--- Table structure for sys_expense
--- ----------------------------
-DROP TABLE IF EXISTS `sys_expense`;
-CREATE TABLE `sys_expense` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `money` decimal(20,2) DEFAULT NULL COMMENT '报销金额',
-  `desc` varchar(255) DEFAULT '' COMMENT '描述',
-  `createtime` datetime DEFAULT CURRENT_TIMESTAMP,
-  `state` int(11) DEFAULT NULL COMMENT '状态: 1.待提交  2:待审核   3.审核通过 4:驳回',
-  `userid` int(11) DEFAULT NULL COMMENT '用户id',
-  `processId` varchar(255) DEFAULT NULL COMMENT '流程定义id',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COMMENT='报销表';
 
 -- ----------------------------
 -- Records of sys_expense
@@ -373,7 +359,7 @@ CREATE TABLE `sys_user` (
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', 'girl.gif', 'admin', 'ecfadcde9305f8891bcfe5a1e28c253e', '8pgby', '张三', '2017-05-05 00:00:00', '2', 'sn93@qq.com', '18200000000', '1', '27', '1', '2016-01-29 08:49:53', '25');
+INSERT INTO `sys_user` VALUES ('1', 'girl.gif', 'admin', 'ecfadcde9305f8891bcfe5a1e28c253e', '8pgby', '张三', '2017-05-05 00:00:00', '2', 'sn93@qq.com', '18200000000', '1', '0', '1', '2016-01-29 08:49:53', '25');
 INSERT INTO `sys_user` VALUES ('44', null, 'test', '45abb7879f6a8268f1ef600e6038ac73', 'ssts3', 'test', '2017-05-01 00:00:00', '1', 'abc@123.com', '', '5', '26', '3', '2017-05-16 20:33:37', null);
 INSERT INTO `sys_user` VALUES ('45', null, 'boss', '71887a5ad666a18f709e1d4e693d5a35', '1f7bf', '老板', '2017-12-04 00:00:00', '1', '', '', '1', '24', '1', '2017-12-04 22:24:02', null);
 INSERT INTO `sys_user` VALUES ('46', null, 'manager', 'b53cac62e7175637d4beb3b16b2f7915', 'j3cs9', '经理', '2017-12-04 00:00:00', '1', '', '', '1', '24', '1', '2017-12-04 22:24:24', null);
